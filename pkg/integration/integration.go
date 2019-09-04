4413da424f1555c0ac53751b117e7aa1401202fa
charles.corbett@wurl.com <nafredy@gmail.com>
2019-09-04 13:30:25 -0700

    origin service integration tests

--- pkg/integration/integration.go
+++ pkg/integration/integration.go
@@ -1,2 +1,18 @@
 // Package integration provides helpers for integration tests
 package integration
+
+import (
+	"fmt"
+	"os"
+)
+
+// GetIntegrationAccountHash fetches the integration account hash from Env var as your
+// target account to test against
+func GetIntegrationAccountHash() (string, error) {
+	accountHash := os.Getenv("INTEGRATIONACCOUNTHASH")
+	if accountHash == "" {
+		return "", fmt.Errorf("INTEGRATIONACCOUNTHASH must be defined to run integration tests")
+	}
+
+	return accountHash, nil
+}
--- striketracker/services/origin/origin_test.go
+++ striketracker/services/origin/origin_test.go
@@ -1,26 +1,184 @@
 package origin
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/openwurl/wurlwind/pkg/integration"
+	"github.com/openwurl/wurlwind/striketracker/models"
 )
 
-var (
-	integrationAccountHash = "z3d5t6j7"
-)
+// setup is called by tests to set up the client for integration testing
+func setup() (*Service, string, error) {
+	c, err := integration.NewIntegrationClient()
+	if err != nil {
+		return nil, "", err
+	}
 
+	s := New(c)
+
+	accountHash, err := integration.GetIntegrationAccountHash()
+	if err != nil {
+		return nil, "", err
+	}
+
+	return s, accountHash, nil
+}
+
+// TestDestructiveSuiteIntegration tests create/update/get/delete
+func TestDestructiveSuiteIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	var testSuite = []struct {
+		name   string
+		create *models.Origin
+		update *models.Origin
+	}{
+		{
+			name: "Destructive Suite Test 01 with path update",
+			create: &models.Origin{
+				Name:     "CUGD Integration Test 01",
+				Hostname: "integration.noroute.com",
+				Port:     80,
+			},
+			update: &models.Origin{
+				Name:     "CUGD Integration Test 01",
+				Hostname: "integration.noroute.com",
+				Port:     80,
+				Path:     "/updated/with/path",
+			},
+		},
+		{
+			name: "Destructive Suite Test 02 with path and port update",
+			create: &models.Origin{
+				Name:     "CUGD Integration Test 02",
+				Hostname: "integration.nohost.com",
+				Port:     80,
+				Path:     "/created/with/path",
+			},
+			update: &models.Origin{
+				Name:     "CUGD Integration Test 02",
+				Hostname: "differenthost.nohost.com",
+				Port:     8080,
+				Path:     "",
+			},
+		},
+	}
+
+	// Run setup
+	s, accountHash, err := setup()
+	if err != nil {
+		t.Fatalf("Expected client to be configured and account hash to be found but received error: %v", err)
+	}
+
+	// Run suite in a loop
+	// Create, Update, Get, Delete
+	for _, tt := range testSuite {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create
+			t.Logf("Create %s", tt.name)
+			createResponse, err := s.Create(accountHash, tt.create)
+			if err != nil {
+				t.Fatalf("Expected creation of integration origin but received error: %v", err)
+			}
+
+			if createResponse == nil {
+				t.Fatalf("Expected to receive struct containing created origin details, but got nil")
+			}
+
+			if createResponse.ID == 0 {
+				t.Fatalf("Did not receive an Origin ID from create response")
+			}
+
+			if createResponse.Name != tt.create.Name {
+				t.Fatalf("Expected created origin to have name %s but has %s", tt.create.Name, createResponse.Name)
+			}
+
+			if createResponse.Hostname != tt.create.Hostname {
+				t.Fatalf("Expected created origin to point to hostname %s but has %s", tt.create.Hostname, createResponse.Hostname)
+			}
+
+			if createResponse.Port != tt.create.Port {
+				t.Fatalf("Expected created origin to use port %d but got %d", tt.create.Port, createResponse.Port)
+			}
+
+			if tt.create.Path == "" {
+				if createResponse.Path != "/" {
+					t.Fatalf("Expected no path on created origin but found %s", createResponse.Path)
+				}
+			} else {
+				if createResponse.Path != tt.create.Path {
+					t.Fatalf("Expected path on created origin to be %s but found %s", tt.create.Path, createResponse.Path)
+				}
+			}
+
+			// Update
+			t.Logf("Update %s", tt.name)
+			update := *tt.update
+			update.ID = createResponse.ID
+			updatedResponse, err := s.Update(accountHash, &update)
+			if err != nil {
+				t.Fatalf("Expected update of integration origin but received error: %v", err)
+			}
+
+			if updatedResponse == nil {
+				t.Fatalf("Expected to receive struct containing updated origin details, but got nil")
+			}
+
+			if tt.update.Path == "" {
+				if updatedResponse.Path != "/" {
+					t.Fatalf("Expected no path on updated origin but found %s", createResponse.Path)
+				}
+			} else {
+				if updatedResponse.Path != tt.update.Path {
+					t.Fatalf("Expected updated path to be [%s] but got [%s]", tt.update.Path, updatedResponse.Path)
+				}
+			}
+
+			if updatedResponse.Port != tt.update.Port {
+				t.Fatalf("Expected updated port to be %d but got %d", tt.update.Port, updatedResponse.Port)
+			}
+
+			// Get
+			t.Logf("Get %s", tt.name)
+			receivedResponse, err := s.Get(accountHash, fmt.Sprintf("%d", updatedResponse.ID))
+			if err != nil {
+				t.Fatalf("Expected integration origin but received error: %v", err)
+			}
+
+			if !reflect.DeepEqual(receivedResponse, updatedResponse) {
+				t.Fatalf("Expected update and get resposes to be identical, but they were not")
+			}
+
+			// Delete
+			t.Logf("Delete %s", tt.name)
+			err = s.Delete(accountHash, fmt.Sprintf("%d", updatedResponse.ID))
+			if err != nil {
+				t.Fatalf("Expected deletion of integration origin but received error: %v", err)
+			}
+		})
+	}
+
+}
+
+// TestCleanupIntegration cleans up the integration account in case of errors
+//func TestCleanupIntegration(t *testing.T) {
+//	t.Log("Not implemented")
+//}
+
+/*
 func TestGetIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
 
-	c, err := integration.NewIntegrationClient()
+	s, accountHash, err := setup()
 	if err != nil {
-		t.Fatalf("Expected client to be configured but received error: %v", err)
+		t.Fatalf("Expected client to be configured and account hash to be found but received error: %v", err)
 	}
-
-	s := New(c)
 
 	var tests = []struct {
 		name          string
@@ -33,7 +191,7 @@ func TestGetIntegration(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			thisOrigin, err := s.Get(integrationAccountHash, tt.originID)
+			thisOrigin, err := s.Get(accountHash, tt.originID)
 			if tt.shouldSucceed {
 				if err != nil {
 					t.Fatalf("Expected to find origin with ID %s, but got error: %v", tt.originID, err)
@@ -46,3 +204,4 @@ func TestGetIntegration(t *testing.T) {
 		})
 	}
 }
+*/
